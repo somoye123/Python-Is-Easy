73328644d621b130e6ca6a6e6c42682cbeb1a34d
somoye123 <somoye.ayotunde@gmail.com>
2021-01-04 17:32:28 +0100

    invoke addOne fuction and print out it values

--- Functions.py
+++ Functions.py
@@ -10,3 +10,11 @@ def addOne(Number):
     Output = Number +1
     return Output
 
+Var = 0
+print(Var)
+Var2 = addOne(Var)
+Var3 = addOne(Var2)
+Var4 = addOne(2.1+3.4)
+print(Var2)
+print(Var3)
+print(Var4)
