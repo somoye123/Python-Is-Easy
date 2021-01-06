95b98fe5149213c291aa9a6a2946bf00f5b72091
somoye123 <somoye.ayotunde@gmail.com>
2021-01-06 09:17:43 +0100

    add two examples of if statement in action

--- IfStatements.py
+++ IfStatements.py
@@ -4,3 +4,20 @@ Structure of If Statements in Python
 if condition:
     Action
 """
+
+click = True
+
+like = 0
+
+if click == True:
+    like += 1
+
+print(like)
+
+Temperature = 15
+Thermo = 25
+
+if Temperature <= 15:
+    Thermo += 5
+
+print(Thermo)
