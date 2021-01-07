cfa50ecaf79097aaa9de1d3f25d46cbf81419d90
somoye123 <somoye.ayotunde@gmail.com>
2021-01-07 15:12:48 +0100

    add a sample list

--- List.py
+++ List.py
@@ -3,3 +3,8 @@ Structure of a python list
 
 TestList = [0,1,2]
 """
+
+Scores = [1,2,3,4,5]
+print(Scores[0:2]) # 0 stands for the start index up to but not including value at index 2
+
+# list can contain several data types
