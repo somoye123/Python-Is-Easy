d276d0da8fca95b4ec592ad73eb7488990c4a691
somoye123 <somoye.ayotunde@gmail.com>
2021-01-07 15:14:27 +0100

    add sample for deleting a list index value

--- List.py
+++ List.py
@@ -8,3 +8,5 @@ Scores = [1,2,3,4,5]
 print(Scores[0:2]) # 0 stands for the start index up to but not including value at index 2
 
 # list can contain several data types
+
+Scores[1:3] = [] # this delete all index values starting from 1 up to but not including index 3
