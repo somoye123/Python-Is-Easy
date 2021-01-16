3eed208820b74bf9e60285ba50235892ec479d6d
somoye123 <somoye.ayotunde@gmail.com>
2021-01-16 15:13:32 +0100

    build tic-tac-toe board using nexted loop

---
+++ NextedLoop.py
@@ -0,0 +1,12 @@
+for row in range(5):
+    if row%2 == 0:
+        for column in range(1,6):
+            if column%2 ==1:
+                if column !=5:
+                    print(" ",end="")
+                else:
+                    print(" ")
+            else:
+                print("|",end="")
+    else:
+        print("-----")
