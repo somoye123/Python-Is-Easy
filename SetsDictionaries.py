ff1be519faccfe52784cfe610f0fa17d8fc8c324
somoye123 <somoye.ayotunde@gmail.com>
2021-01-29 20:48:50 +0100

    update the while loop example for dictionaries

--- SetsDictionaries.py
+++ SetsDictionaries.py
@@ -4,4 +4,11 @@
 BlackShoes = {42:2,41:3,40:4,39:1,38:0}
 print(BlackShoes)
 while(True):
-    purchaseSize = int(input("Which shoe size would you like to buy"))
+    purchaseSize = int(input("Which shoe size would you like to buy?\n"))
+    if purchaseSize < 0:
+        break
+    if BlackShoes[purchaseSize] > 0:
+        BlackShoes[purchaseSize] -=1
+    else:
+        print("Shoes are no longer in stock")
+    print(BlackShoes)
